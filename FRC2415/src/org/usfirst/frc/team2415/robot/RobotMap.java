47ed6635321b86e2776903a89f28949081102d4d
Arxos0012 <booker.oronde@gmail.com>
2017-03-31 16:14:58 -0400

    Updated ports

--- FRC2415/src/org/usfirst/frc/team2415/robot/RobotMap.java
+++ FRC2415/src/org/usfirst/frc/team2415/robot/RobotMap.java
@@ -24,12 +24,11 @@ public class RobotMap {
 	public static final int RIGHT_TALON_BACK = 3; 
 	public static final int RIGHT_TALON_FRONT = 4;
   
-	public static final int INTAKE_TALON = 5;
 	public static final int SHOOTER_TALON = 6; 
 	public static final int FEEDER_TALON = 7;
 	public static final int CAROUSEL_TALON = 8;
 	public static final int CLIMBER_TALON = 9;
-	public static final int CLIMBER_TALON2 = 0;
+	public static final int CLIMBER_TALON2 = 5;
 	
 	public static final int[] GEAR_MANIP_SOLENOID = {2,4};
 	public static final int GEAR_POKE_SOLENOID = 0;
