f7a0aba28e5372b39d9dd1ed2aa52e26ef13b4df
Omari Matthews <omatthews@hmc.edu>
2018-01-22 00:44:45 -0800

    Implemented CheesyDrive in the ArcadeDriveCommand

--- FRC2415/src/org/usfirst/frc/team2415/robot/commands/ArcadeDriveCommand.java
+++ FRC2415/src/org/usfirst/frc/team2415/robot/commands/ArcadeDriveCommand.java
@@ -4,84 +4,60 @@ import org.usfirst.frc.team2415.robot.Robot;
 
 import com.ctre.CANTalon.TalonControlMode;
 
+import cheesy.CheesyDriveHelper;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class ArcadeDriveCommand extends Command {
-	
-	private double INTERPOLATION_FACTOR = 0.75;
-	private double DEADBAND = 0.05;
-	private double STRAIGHT_RESTRICTER = 1; 
-	private double TURN_SPEED_BOOST = 0.4;
-	private double overPower = .6;
 
-    public ArcadeDriveCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.driveSubsystem);
-    }
+	private CheesyDriveHelper cheesyDriveHelper = new CheesyDriveHelper();
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.driveSubsystem.changeControlMode(TalonControlMode.PercentVbus);
-    	Robot.driveSubsystem.stopMotors();
-    }
+	public ArcadeDriveCommand() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.driveSubsystem);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	
-    	double leftY;
-    	double rightX;
-    	
-    	if (Robot.singlePlayerMode){
-    		leftY = Robot.operator.Y();
-    		rightX = Robot.operator.X();
-    	}	else {
-    		leftY = Robot.gamepad.leftY();
-    		rightX = Robot.gamepad.rightX();
-    	}
-    	
-    	if (Math.abs(leftY) < DEADBAND) leftY = 0;
-    	if (Math.abs(rightX) < DEADBAND) rightX = 0;
-    	
-    	leftY = INTERPOLATION_FACTOR*Math.pow(leftY, 3) + (1-INTERPOLATION_FACTOR)*leftY;
-    	rightX = INTERPOLATION_FACTOR*Math.pow(rightX, 3) + (1-INTERPOLATION_FACTOR)*rightX;
-    	
-    	double left = STRAIGHT_RESTRICTER*leftY + TURN_SPEED_BOOST*rightX;
-    	double right = STRAIGHT_RESTRICTER*leftY - TURN_SPEED_BOOST*rightX;
-    	
-        if (left > 1.0) {
-            right -= overPower * (left - 1.0);
-            left = 1.0;
-        } else if (right > 1.0) {
-            left -= overPower * (right - 1.0);
-            right = 1.0;
-        } else if (left < -1.0) {
-            right += overPower * (-1.0 - left);
-            left = -1.0;
-        } else if (right < -1.0) {
-            left += overPower * (-1.0 - right);
-            right = -1.0;
-        }
-    	
-    	Robot.driveSubsystem.setMotors(left, right);
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.driveSubsystem.changeControlMode(TalonControlMode.PercentVbus);
+		Robot.driveSubsystem.stopMotors();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.driveSubsystem.stopMotors();
-    }
+		double leftY;
+		double rightX;
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	Robot.driveSubsystem.stopMotors();
-    }
+		if (Robot.singlePlayerMode) {
+			leftY = Robot.operator.Y();
+			rightX = Robot.operator.X();
+		} else {
+			leftY = Robot.gamepad.leftY();
+			rightX = Robot.gamepad.rightX();
+		}
+
+		boolean isQuickTurn = leftY < 0.1;
+
+		Robot.driveSubsystem.setMotors(cheesyDriveHelper.cheesyDrive(leftY, rightX, isQuickTurn, false));
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.driveSubsystem.stopMotors();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		Robot.driveSubsystem.stopMotors();
+	}
 }
--- FRC2415/src/org/usfirst/frc/team2415/robot/subsystems/DriveSubsystem.java
+++ FRC2415/src/org/usfirst/frc/team2415/robot/subsystems/DriveSubsystem.java
@@ -6,10 +6,10 @@ import org.usfirst.frc.team2415.robot.utilities.PixyCam;
 
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
-import com.ctre.CANTalon.StatusFrameRate;
 import com.ctre.CANTalon.TalonControlMode;
 import com.kauailabs.navx.frc.AHRS;
 
+import cheesy.DriveSignal;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -18,174 +18,181 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class DriveSubsystem extends Subsystem {
-	
+
 	private CANTalon leftTalBack, leftTalFront, rightTalBack, rightTalFront;
 	public AHRS ahrs;
 	private PixyCam pixy;
-	
+
 	public boolean isMoving;
-	
+
 	public final double WHEEL_CIRCUMFERENCE = .270833333 * Math.PI; // 3.25 inch
-	final double GEAR_RATIO = 1/4.909090909; // Reduction from encoder shaft and output shaft
+	final double GEAR_RATIO = 1 / 4.909090909; // Reduction from encoder shaft and output shaft
 	final double PULSES_PER_REVOLUTION = 4096.0; // Number of encoder counts per revolution
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    	setDefaultCommand(new VelocityDriveCommand());
-    }
-    
-    public DriveSubsystem() {
-    	
-    	try {
-            /* Communicate w/navX-MXP via the MXP SPI Bus.                                     */
-            /* Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB     */
-            /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
-            ahrs = new AHRS(SerialPort.Port.kMXP); 
-        } catch (RuntimeException ex ) {
-            DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
-        }
-    	
-    	leftTalBack = new CANTalon(RobotMap.LEFT_TALON_BACK);
-    	leftTalFront = new CANTalon(RobotMap.LEFT_TALON_FRONT);
-    	rightTalBack = new CANTalon(RobotMap.RIGHT_TALON_BACK);
-    	rightTalFront = new CANTalon(RobotMap.RIGHT_TALON_FRONT);
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+		setDefaultCommand(new ArcadeDriveCommand());
+	}
 
-    	leftTalBack.changeControlMode(TalonControlMode.Follower);
-    	leftTalBack.set(leftTalFront.getDeviceID());
-    	rightTalBack.changeControlMode(TalonControlMode.Follower);
-    	rightTalBack.set(rightTalFront.getDeviceID());
-    	
-    	leftTalFront.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-    	rightTalFront.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-    	
-    	leftTalFront.enableBrakeMode(false);
-    	rightTalFront.enableBrakeMode(false);
-    	
-    }
-    
-    public void stopMotors() {
-    	leftTalFront.set(0);
-    	rightTalFront.set(0);
-    }
-    
-    public void setMotors(double left, double right) {
-     	leftTalFront.set(-left);
-     	rightTalFront.set(right);
-    }
-    
-    public void setLeft(double speed){
-    	leftTalFront.set(-speed);
-    }
+	public DriveSubsystem() {
 
-    public void setRight(double speed){
-    	rightTalFront.set(speed);
-    }
-    
-    public void setBreakMode(boolean mode){
-    	leftTalFront.enableBrakeMode(mode);
-    	rightTalFront.enableBrakeMode(mode);
-    	leftTalBack.enableBrakeMode(mode);
-    	rightTalBack.enableBrakeMode(mode);
-    }
-    
-    public void setTalonLimits(double max){
-    	leftTalBack.configPeakOutputVoltage(max, -max);
-    	rightTalBack.configPeakOutputVoltage(max, -max);
-    }
-    
-    public void changeControlMode(TalonControlMode mode){
-    	leftTalFront.changeControlMode(mode);
-    	rightTalFront.changeControlMode(mode);
-    	
-    	if(mode == TalonControlMode.Speed){
-    		//TODO: see 12.4
-    		leftTalFront.configNominalOutputVoltage(0, 0);
-    		leftTalFront.configPeakOutputVoltage(12, -12);
-    		setPIDF(leftTalFront, .1696969696, 0, 0, 2/1000);
+		try {
+			/* Communicate w/navX-MXP via the MXP SPI Bus. */
+			/* Alternatively: I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB */
+			/*
+			 * See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for
+			 * details.
+			 */
+			ahrs = new AHRS(SerialPort.Port.kMXP);
+		} catch (RuntimeException ex) {
+			DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
+		}
 
-    		rightTalFront.configNominalOutputVoltage(0, 0);
-    		rightTalFront.configPeakOutputVoltage(12, -12);
-    		setPIDF(rightTalFront, .175, 0, 0, 2/1000);
+		leftTalBack = new CANTalon(RobotMap.LEFT_TALON_BACK);
+		leftTalFront = new CANTalon(RobotMap.LEFT_TALON_FRONT);
+		rightTalBack = new CANTalon(RobotMap.RIGHT_TALON_BACK);
+		rightTalFront = new CANTalon(RobotMap.RIGHT_TALON_FRONT);
 
-    	} else if(mode == TalonControlMode.Position){
-    		leftTalFront.configNominalOutputVoltage(0, 0);
-    		leftTalFront.configPeakOutputVoltage(12, -12);
-    		setPIDF(leftTalFront, 1.125, 0, 0, 0);
+		leftTalBack.changeControlMode(TalonControlMode.Follower);
+		leftTalBack.set(leftTalFront.getDeviceID());
+		rightTalBack.changeControlMode(TalonControlMode.Follower);
+		rightTalBack.set(rightTalFront.getDeviceID());
 
-    		rightTalFront.configNominalOutputVoltage(0, 0);
-    		rightTalFront.configPeakOutputVoltage(12, -12);
-    		setPIDF(rightTalFront, 1.125, 0, 0, 0);
-    	}
-    }
-    
-    public void setPIDF(CANTalon talon, double kP, double kI, double kD, double kF){
-    	talon.setProfile(0);
-    	talon.setPID(kP, kI, kD);
-    	talon.setF(kF);
-    }
-    
-    public double getPitch(){
-    	return ahrs.getPitch();
-    }
-    
-    public double getYaw(){
-    	return ahrs.getYaw();
-    }
-    
-    public void zeroYaw(){
-    	ahrs.zeroYaw();
-    }
-    
-    public double getRoll(){
-    	return ahrs.getRoll();
-    }
-    
-    public double getAngle(){
-    	return ahrs.getAngle();
-    }
-    
-    public double fPS2RPM(double fps){
-    	return (fps*60)/(WHEEL_CIRCUMFERENCE);
-    }
-    
-    public double[] getDistance(){
-    	return new double[]{leftTalFront.getPosition()*WHEEL_CIRCUMFERENCE,
-							rightTalFront.getPosition()*WHEEL_CIRCUMFERENCE};
-    }
-    
-    public void zeroEncoders(){
-    	leftTalFront.setPosition(0);
-    	rightTalFront.setPosition(0);
-    }
-    
-    public double[] getVelocity(){
-    	return new double[]{leftTalFront.getSpeed(), rightTalFront.getSpeed()};
-    }
-    
-    public double[] getError(){
-    	return new double[]{leftTalFront.getClosedLoopError(), rightTalFront.getClosedLoopError()};
-    }
-    
-    //no slip @ 27.4285714286
-    public void setLeftRampRate(double rate){
+		leftTalFront.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		rightTalFront.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+
+		leftTalFront.enableBrakeMode(false);
+		rightTalFront.enableBrakeMode(false);
+
+	}
+
+	public void stopMotors() {
+		leftTalFront.set(0);
+		rightTalFront.set(0);
+	}
+
+	public void setMotors(double left, double right) {
+		leftTalFront.set(-left);
+		rightTalFront.set(right);
+	}
+
+	public void setMotors(DriveSignal signal) {
+		leftTalonFront.set(signal.getLeft());
+		rightTalonFront.set(-signal.getRight());
+	}
+
+	public void setLeft(double speed) {
+		leftTalFront.set(-speed);
+	}
+
+	public void setRight(double speed) {
+		rightTalFront.set(speed);
+	}
+
+	public void setBreakMode(boolean mode) {
+		leftTalFront.enableBrakeMode(mode);
+		rightTalFront.enableBrakeMode(mode);
+		leftTalBack.enableBrakeMode(mode);
+		rightTalBack.enableBrakeMode(mode);
+	}
+
+	public void setTalonLimits(double max) {
+		leftTalBack.configPeakOutputVoltage(max, -max);
+		rightTalBack.configPeakOutputVoltage(max, -max);
+	}
+
+	public void changeControlMode(TalonControlMode mode) {
+		leftTalFront.changeControlMode(mode);
+		rightTalFront.changeControlMode(mode);
+
+		if (mode == TalonControlMode.Speed) {
+			// TODO: see 12.4
+			leftTalFront.configNominalOutputVoltage(0, 0);
+			leftTalFront.configPeakOutputVoltage(12, -12);
+			setPIDF(leftTalFront, .1696969696, 0, 0, 2 / 1000);
+
+			rightTalFront.configNominalOutputVoltage(0, 0);
+			rightTalFront.configPeakOutputVoltage(12, -12);
+			setPIDF(rightTalFront, .175, 0, 0, 2 / 1000);
+
+		} else if (mode == TalonControlMode.Position) {
+			leftTalFront.configNominalOutputVoltage(0, 0);
+			leftTalFront.configPeakOutputVoltage(12, -12);
+			setPIDF(leftTalFront, 1.125, 0, 0, 0);
+
+			rightTalFront.configNominalOutputVoltage(0, 0);
+			rightTalFront.configPeakOutputVoltage(12, -12);
+			setPIDF(rightTalFront, 1.125, 0, 0, 0);
+		}
+	}
+
+	public void setPIDF(CANTalon talon, double kP, double kI, double kD, double kF) {
+		talon.setProfile(0);
+		talon.setPID(kP, kI, kD);
+		talon.setF(kF);
+	}
+
+	public double getPitch() {
+		return ahrs.getPitch();
+	}
+
+	public double getYaw() {
+		return ahrs.getYaw();
+	}
+
+	public void zeroYaw() {
+		ahrs.zeroYaw();
+	}
+
+	public double getRoll() {
+		return ahrs.getRoll();
+	}
+
+	public double getAngle() {
+		return ahrs.getAngle();
+	}
+
+	public double fPS2RPM(double fps) {
+		return (fps * 60) / (WHEEL_CIRCUMFERENCE);
+	}
+
+	public double[] getDistance() {
+		return new double[] { leftTalFront.getPosition() * WHEEL_CIRCUMFERENCE,
+				rightTalFront.getPosition() * WHEEL_CIRCUMFERENCE };
+	}
+
+	public void zeroEncoders() {
+		leftTalFront.setPosition(0);
+		rightTalFront.setPosition(0);
+	}
+
+	public double[] getVelocity() {
+		return new double[] { leftTalFront.getSpeed(), rightTalFront.getSpeed() };
+	}
+
+	public double[] getError() {
+		return new double[] { leftTalFront.getClosedLoopError(), rightTalFront.getClosedLoopError() };
+	}
+
+	// no slip @ 27.4285714286
+	public void setLeftRampRate(double rate) {
 		leftTalFront.setVoltageRampRate(rate);
-    }
-    
-    public void setRightRampRate(double rate){
-		rightTalFront.setVoltageRampRate(rate);
-    }
-    
-    public double getVoltage(){
-    	return  leftTalFront.getOutputVoltage()/leftTalFront.getBusVoltage();
-    }
-    
-    public void updateStatus() {
-    	
-    }
-   
-}
+	}
 
+	public void setRightRampRate(double rate) {
+		rightTalFront.setVoltageRampRate(rate);
+	}
+
+	public double getVoltage() {
+		return leftTalFront.getOutputVoltage() / leftTalFront.getBusVoltage();
+	}
+
+	public void updateStatus() {
+
+	}
+
+}
